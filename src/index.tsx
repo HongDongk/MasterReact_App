10ff05500d67aea5c5b2a6d1c2f509ecce061881
Dongkeun <64244383+HongDongk@users.noreply.github.com>
2022-03-22 16:25:06 +0900

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { theme } from "./theme";
+import { ThemeProvider } from "styled-components";
 
 ReactDOM.render(
   
   <React.StrictMode>
-     <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
