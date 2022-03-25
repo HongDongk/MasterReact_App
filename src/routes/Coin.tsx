e8336896878f2fdef336568a285eac2982a085fe
Dongkeun <hongdongk@naver.com>
2022-03-25 16:49:29 +0900

    Final

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import {Switch,Route,useLocation,useParams,useRouteMatch,Link} from "react-router-dom";
 import { useQuery } from "react-query";
 import styled from "styled-components";
@@ -140,22 +140,21 @@ function Coin() {
   const priceMatch = useRouteMatch("/:coinId/price");
   const chartMatch = useRouteMatch("/:coinId/chart");
 
-  const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(
-    ["info", coinId],
-    () => fetchCoinInfo(coinId)
-  );
-  const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
-    ["tickers", coinId],
-    () => fetchCoinTickers(coinId)
-  );
+  const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(["info", coinId], () => fetchCoinInfo(coinId));
+  const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(["tickers", coinId], () => fetchCoinTickers(coinId),{refetchInterval:5000});
   const loading = infoLoading || tickersLoading;
   
   return (
     <Container>
+      <Helmet>
+        <title>
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </title>
+      </Helmet>
       <Header>
       <Title>
           {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
-        </Title>
+      </Title>
       </Header>
       {loading ? (
         <Loader>Loading...</Loader>
@@ -171,8 +170,8 @@ function Coin() {
               <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
-              <span>Open Source:</span>
-              <span>{infoData?.open_source ? "Yes" : "No"}</span>
+              <span>Price:</span>
+              <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
@@ -201,7 +200,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} /> 
             </Route>
           </Switch>
         </>
